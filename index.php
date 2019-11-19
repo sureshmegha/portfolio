cf7d2cd309ab2292c8acb896e0ca2a7448617552
sureshmegha <meghasuresh12@gmail.com>
2019-11-19 16:26:15 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+  <head> 
+    <title> PHP Test </title>
+  </head>
+  <body>
+  <?php echo '<p> Hello World! Here are all of my links [Megha Suresh CSP Period 1] </p>'; ?>
+    <li> <a  href="index.html">Home</a> </li> </br> 
+    <li> <a href="Suresh_AboutMe.html">About Me</a> </li> </br>  
+    <li> <a href="Suresh_ContactMe.html">Contact Me</a> </li> </br>  
+    <li> <a href="Suresh_Projects.html">Projects</a> </li> </br>  
+    <li> <a href="Suresh_Resume.html">Resume</a> </li> 
+  </body>
